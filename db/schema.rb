82744769a6948bed64c918ae54febb62c3d68854
shu913 <sss91327@icloud.com>
2023-07-02 15:47:25 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_161958) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.integer "postcode"
     t.string "prefecture"
     t.string "address_city"
     t.string "address_street"
     t.string "address_building"
     t.text "profile"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
